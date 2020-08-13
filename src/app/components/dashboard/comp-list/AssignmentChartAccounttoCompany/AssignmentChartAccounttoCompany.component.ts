ffc665e1017f10321605e52b1787a331ac3acc20
KrishnaPrasad.Gedda@xpo.com <KrishnaPrasad.Gedda@xpo.com>
2020-08-13 22:03:08 +0530

    Code Review Changes

--- src/app/components/dashboard/comp-list/AssignmentChartAccounttoCompany/AssignmentChartAccounttoCompany.component.ts
+++ src/app/components/dashboard/comp-list/AssignmentChartAccounttoCompany/AssignmentChartAccounttoCompany.component.ts
@@ -1,5 +1,4 @@
 import { Component, Inject, Optional, OnInit } from '@angular/core';
-import { AlertService } from '../../../../services/alert.service';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { isNullOrUndefined } from 'util';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -8,13 +7,8 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { ApiConfigService } from '../../../../services/api-config.service';
 import { ApiService } from '../../../../services/api.service';
 import { String } from 'typescript-string-operations';
-import { CommonService } from '../../../../services/common.service';
 import { AddOrEditService } from '../add-or-edit.service';
 
-interface Status {
-  value: string;
-  viewValue: string;
-}
 @Component({
   selector: 'app-AssignmentChartAccounttoCompany',
   templateUrl: './AssignmentChartAccounttoCompany.component.html',
@@ -24,47 +18,33 @@ interface Status {
 export class AssignmentChartAccounttoCompanyComponent implements OnInit {
 
   modelFormData: FormGroup;
-  isSubmitted = false;
-  formData: any;
-  taxcodeList: any;
-  taxaccList: any;
-  tdsList:any;
-  incmList:any;
-  coaList: any;
-  status: Status[] =
-  [
-    { value: '1', viewValue: 'Resident' },
-    { value: '2', viewValue: 'Non Resident' }
-  ];
+  formData: any;  
+  operationCoaList: any;  
+  groupCoaList: any;  
   companyList: any;
+
   constructor(
-    private alertService: AlertService,
     private addOrEditService: AddOrEditService,
     private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<AssignmentChartAccounttoCompanyComponent>,
     private spinner: NgxSpinnerService,
     private apiConfigService: ApiConfigService,
     private apiService: ApiService,
-    private commonService: CommonService,
     // @Optional() is used to prevent error if no data is passed
     @Optional() @Inject(MAT_DIALOG_DATA) public data: any) {
 
     this.modelFormData = this.formBuilder.group({
       code: [0],
-      // description: [null, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
       operationCoa: [null],
       groupCoa: [null],
-      company: [null]
-      
+      company: [null]      
     });
-
 
     this.formData = { ...data };
     if (!isNullOrUndefined(this.formData.item)) {
       this.modelFormData.patchValue(this.formData.item);
       this.modelFormData.controls['code'].disable();
     }
-
   }
 
   ngOnInit() {
@@ -95,7 +75,8 @@ export class AssignmentChartAccounttoCompanyComponent implements OnInit {
           const res = response.body;
           if (!isNullOrUndefined(res) && res.status === StatusCodes.pass) {
             if (!isNullOrUndefined(res.response)) {
-              this.coaList = res.response['coaList'];
+              this.operationCoaList = res.response['coaList'].filter(res => res.type == 'Operational');
+              this.groupCoaList = res.response['coaList'].filter(res => res.type == 'Consolidated');
             }
           }
           this.spinner.hide();
@@ -103,7 +84,6 @@ export class AssignmentChartAccounttoCompanyComponent implements OnInit {
   }
 
   get formControls() { return this.modelFormData.controls; }
-
 
   save() {
     if (this.modelFormData.invalid) {
