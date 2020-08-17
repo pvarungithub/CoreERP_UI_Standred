bf3e8336f05d58e3a81d741d05f3fd91827e8dbc
KrishnaPrasad.Gedda@xpo.com <KrishnaPrasad.Gedda@xpo.com>
2020-08-17 18:18:39 +0530

    Sub Asset as tab

--- src/app/components/dashboard/comp-list/subassets/subassets.component.ts
+++ src/app/components/dashboard/comp-list/subassets/subassets.component.ts
@@ -1,15 +1,16 @@
-import { Component, Inject, Optional, OnInit } from '@angular/core';
-import { AlertService } from '../../../../services/alert.service';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { isNullOrUndefined } from 'util';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { StatusCodes } from '../../../../enums/common/common';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { ApiConfigService } from '../../../../services/api-config.service';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../../../services/api.service';
 import { String } from 'typescript-string-operations';
-import { CommonService } from '../../../../services/common.service';
+import { isNullOrUndefined } from 'util';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ApiConfigService } from '../../../../services/api-config.service';
+import { StatusCodes } from '../../../../enums/common/common';
 import { AddOrEditService } from '../add-or-edit.service';
+import { AlertService } from '../../../../services/alert.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonService } from '../../../../services/common.service';
+
 
 
 @Component({
@@ -40,16 +41,18 @@ export class SubAssetsComponent implements OnInit {
   companyList:any;
  
   constructor(
-    private alertService: AlertService,
-    private addOrEditService: AddOrEditService,
-    private formBuilder: FormBuilder,
-    public dialogRef: MatDialogRef<SubAssetsComponent>,
-    private spinner: NgxSpinnerService,
-    private apiConfigService: ApiConfigService,
     private apiService: ApiService,
-    private commonService: CommonService,
+    private addOrEditService: AddOrEditService,
+    private apiConfigService: ApiConfigService,
+    private spinner: NgxSpinnerService,
+    private formBuilder: FormBuilder,
+    private alertService: AlertService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,   
+    private commonService: CommonService
+  ){
     // @Optional() is used to prevent error if no data is passed
-    @Optional() @Inject(MAT_DIALOG_DATA) public data: any) {
+    // @Optional() @Inject(MAT_DIALOG_DATA) public data: any) {
 
     this.modelFormData = this.formBuilder.group({
       subAssetNumber: [null, [Validators.required, Validators.minLength(1), Validators.maxLength(7)]],
@@ -78,7 +81,7 @@ export class SubAssetsComponent implements OnInit {
     });
 
 
-    this.formData = { ...data };
+    this.formData = {...this.addOrEditService.editData };
     if (!isNullOrUndefined(this.formData.item)) {
       this.modelFormData.patchValue(this.formData.item);
       this.modelFormData.controls['subAssetNumber'].disable();
@@ -285,7 +288,7 @@ export class SubAssetsComponent implements OnInit {
     this.modelFormData.controls['subAssetNumber'].enable();
     this.formData.item = this.modelFormData.value;
     this.addOrEditService[this.formData.action](this.formData, (res) => {
-      this.dialogRef.close(this.formData);
+      this.router.navigate(['/dashboard/master/mainassetmaster']);
     });
     if (this.formData.action == 'Edit') {
       this.modelFormData.controls['subAssetNumber'].disable();
@@ -293,7 +296,7 @@ export class SubAssetsComponent implements OnInit {
   }
 
   cancel() {
-    this.dialogRef.close();
+    this.router.navigate(['/dashboard/master/mainassetmaster']);
   }
 
 }
