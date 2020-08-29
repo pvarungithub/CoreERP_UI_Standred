12f1abd35ce0b0fc811bac89fe5b815e28f7e897
KrishnaPrasad.Gedda@xpo.com <KrishnaPrasad.Gedda@xpo.com>
2020-08-29 18:00:18 +0530

    Deleted Unused API Calls

--- src/app/components/dashboard/comp-list/erpuser/erpuser.componet.ts
+++ src/app/components/dashboard/comp-list/erpuser/erpuser.componet.ts
@@ -1,5 +1,4 @@
 import { Component, Inject, Optional, OnInit } from '@angular/core';
-import { AlertService } from '../../../../services/alert.service';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { isNullOrUndefined } from 'util';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -8,8 +7,6 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { ApiConfigService } from '../../../../services/api-config.service';
 import { ApiService } from '../../../../services/api.service';
 import { String } from 'typescript-string-operations';
-import { CommonService } from '../../../../services/common.service';
-
 
 @Component({
   selector: 'app-erpuser',
@@ -18,51 +15,43 @@ import { CommonService } from '../../../../services/common.service';
 })
 
 export class ErpUsersComponent implements OnInit {
-
   modelFormData: FormGroup;
   isSubmitted = false;
   formData: any;
   companyList: any;
-
   employeesList: any;
-    GetBranchesListArray: any;
-    RolesList: any;
+  RolesList: any;
 
   constructor(
     private apiService: ApiService,
     private apiConfigService: ApiConfigService,
     private spinner: NgxSpinnerService,
-    private alertService: AlertService,
     private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<ErpUsersComponent>,
-    private commonService: CommonService,
     // @Optional() is used to prevent error if no data is passed
     @Optional() @Inject(MAT_DIALOG_DATA) public data: any) {
-
     this.modelFormData = this.formBuilder.group({
       userName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(4)]],
       password: ['', [Validators.required, Validators.minLength(2)]],
       role: [null],
-      seqId:['0'],
+      seqId: ['0'],
       branchCode: [null],
       companyCode: [null],
       addDate: [null]
     });
-
 
     this.formData = { ...data };
     if (!isNullOrUndefined(this.formData.item)) {
       this.modelFormData.patchValue(this.formData.item);
       this.modelFormData.controls['userName'].disable();
     }
-
   }
 
   ngOnInit() {
     this.getRolesList();
     this.getTableData();
-    this.getCashPaymentBranchesList();
   }
+
   getRolesList() {
     const getRolesListsUrl = String.Join('/', this.apiConfigService.getrolelist);
     this.apiService.apiGetRequest(getRolesListsUrl).subscribe(
@@ -78,21 +67,7 @@ export class ErpUsersComponent implements OnInit {
         }
       });
   }
-  getCashPaymentBranchesList() {
-    const getCashPaymentBranchesListUrl = String.Join('/', this.apiConfigService.getCashPaymentBranchesList);
-    this.apiService.apiGetRequest(getCashPaymentBranchesListUrl).subscribe(
-      response => {
-        const res = response.body;
-        if (!isNullOrUndefined(res) && res.status === StatusCodes.pass) {
-          if (!isNullOrUndefined(res.response)) {
-            if (!isNullOrUndefined(res.response['BranchesList']) && res.response['BranchesList'].length) {
-              this.GetBranchesListArray = res.response['BranchesList'];
-              this.spinner.hide();
-            }
-          }
-        }
-      });
-  }
+
   getTableData() {
     const getCompanyUrl = String.Join('/', this.apiConfigService.getCompanysList);
     this.apiService.apiGetRequest(getCompanyUrl)
@@ -107,9 +82,8 @@ export class ErpUsersComponent implements OnInit {
           this.spinner.hide();
         });
   }
- 
-  get formControls() { return this.modelFormData.controls; }
 
+  get formControls() { return this.modelFormData.controls; }
 
   save() {
     if (this.modelFormData.invalid) {
@@ -123,5 +97,4 @@ export class ErpUsersComponent implements OnInit {
   cancel() {
     this.dialogRef.close();
   }
-
 }
