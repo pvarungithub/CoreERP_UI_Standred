3850a814bf5de0d515499a2041f1bb7d4f867109
gkprasad27 <krishnaprasadg81@gmail.com>
2023-12-08 12:02:32 +0530

    issue

--- src/app/components/dashboard/trans-list/purcahseorder/preview/preview.component.ts
+++ src/app/components/dashboard/trans-list/purcahseorder/preview/preview.component.ts
@@ -18,30 +18,37 @@ export class PreviewComponent {
 
 
   print() {
-    // this.spinner.show();
-    // setTimeout(() => {
-    let DATA: any = document.getElementById('printData');
-    const WindowPrt = window.open('', '', 'left=0,top=0,width=900,height=900,toolbar=0,scrollbars=0,status=0');
-    WindowPrt.document.write(DATA.innerHTML);
-    WindowPrt.document.close();
-    WindowPrt.focus();
-    WindowPrt.print();
-    WindowPrt.close();
-    // html2canvas(DATA).then((canvas: any) => {
-    //   // let fileWidth = 208;
-    //   // let fileHeight = 279.4;
-    //   // let fileHeight = (canvas.height * fileWidth) / canvas.width;
-    //   const FILEURI = canvas.toDataURL('image/png');
-    //   let PDF = new jsPDF('p', 'mm', 'a4');
-    //   let position = 0;
-    //   var width = PDF.internal.pageSize.getWidth();
-    //   var height = PDF.internal.pageSize.getHeight();
-    //   PDF.addImage(FILEURI, 'PNG', 0, position, width, height);
-    //   PDF.save(`${this.data.heading}.pdf`);
-    //   this.spinner.hide();
-    // });
-    // });
+ 
+    var w = window.open();
+    var html = document.getElementById('printData').innerHTML;
+    w.document.body.innerHTML = html;
+    w.print();
   }
+//   print() {
+//     // this.spinner.show();
+//     // setTimeout(() => {
+//     let DATA: any = document.getElementById('printData');
+//     const WindowPrt = window.open('', '', 'left=0,top=0,width=900,height=900,toolbar=0,scrollbars=0,status=0');
+//     WindowPrt.document.write(DATA.innerHTML);
+//     WindowPrt.document.close();
+//     WindowPrt.focus();
+//     WindowPrt.print();
+//     WindowPrt.close();
+//     // html2canvas(DATA).then((canvas: any) => {
+//     //   // let fileWidth = 208;
+//     //   // let fileHeight = 279.4;
+//     //   // let fileHeight = (canvas.height * fileWidth) / canvas.width;
+//     //   const FILEURI = canvas.toDataURL('image/png');
+//     //   let PDF = new jsPDF('p', 'mm', 'a4');
+//     //   let position = 0;
+//     //   var width = PDF.internal.pageSize.getWidth();
+//     //   var height = PDF.internal.pageSize.getHeight();
+//     //   PDF.addImage(FILEURI, 'PNG', 0, position, width, height);
+//     //   PDF.save(`${this.data.heading}.pdf`);
+//     //   this.spinner.hide();
+//     // });
+//     // });
+//   }
 
   convertNumberToWords(data) {
      var words = new Array();
